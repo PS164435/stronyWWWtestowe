9ee7f6a59059e852bf9e02a4ff916dc30989a6c8
PS164435 <100274100+PS164435@users.noreply.github.com>
2024-01-23 20:33:02 +0100

    23.01. 20:22

---
+++ WWW18/adminsite.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php
+include("index.php");
+include('cfg.php');
+
+?> 
+<body class="<?php echo $backgroundClass; ?>"  onload="startclock()"> <!-- wywołanie tła i zegar -->
+	<script src="./js/kolorujtlo.js" type="text/javascript"></script>
+	<script src="./js/timedate.js" type="text/javascript"></script>
+	<script src="./js/jquery-3.7.1.js"></script>
+<style> <?php
+	include 'css/stylowa.css';
+?> </style> <?php
+
+
+	if (isset($_SESSION["adminlogin"])) {
+		
+		ListaPodstron();
+		DrzewoKategorii($conn);
+		ListaProduktow($conn);
+		
+		
+	}
+
+	?><br><br><?php
+	
+	$nr_indeksu = '164435';
+    $nrGrupy = '4ISI';
+    echo 'Autor: Jacek Szymański' . $nr_indeksu . ' grupa ' . $nrGrupy . '<br><br><br><br>';
+
+?>
